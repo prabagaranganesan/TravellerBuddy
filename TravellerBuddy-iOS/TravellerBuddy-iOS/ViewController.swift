bf4539d81d3f1ae5b344adb7140f226c5f9e43ec
Prabagaran G <nikprabha90@gmail.com>
2023-12-20 13:30:22 +0530

    Create project and folders

---
+++ TravellerBuddy-iOS/TravellerBuddy-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravellerBuddy-iOS
+//
+//  Created by Prabhagaran Ganesan on 20/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
